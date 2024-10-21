97bd6a27e2c81bc07d1f25871401e3e4ea0c26d7
HongYu-lab <jordan87150699@gmail.com>
2024-10-21 17:40:02 +0800

    修改config/db中的password

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+/*
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+*/
 
 app.use('/', index);
 
--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const dbConfig = {
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '' || "root",
     database: 'HoneyTrainDB',
     multipleStatements: true,
 };
